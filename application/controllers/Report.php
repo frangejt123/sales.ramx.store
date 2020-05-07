9cfdff5924df3482fa99e78430b8b98683200ea6
nhojener <nhojener@gmail.com>
2020-05-07 21:19:12 +0800

    Update Report.php
    Add report "Sales Order List By Delivery Date"

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -114,4 +114,42 @@ class Report extends CI_Controller {
 			
 		}
 	}
+
+	public function so_list_by_delivery_date() {
+		$input_param = [
+			"delivery_date_from" => $this->input->get("delivery_date_from", TRUE),
+			"delivery_date_to" => $this->input->get("delivery_date_to", TRUE),
+			"txn_status" => $this->input->get("txn_status", TRUE),
+			"payment_method" => $this->input->get("payment_method", TRUE),
+			"txn_paid" => $this->input->get("txn_paid", TRUE),
+		];
+
+		// Check required parameters
+		if (is_null($input_param["delivery_date_from"]) || is_null($input_param["delivery_date_from"])) $this->output->set_status_header(400)->set_output("Bad Request");
+
+		$report_param = [
+			"DELIVERY_DATE_FROM" => $input_param["delivery_date_from"],
+			"DELIVERY_DATE_TO" => $input_param["delivery_date_to"],
+			"CONDITION" => "transaction.delivery_date>='" . $input_param["delivery_date_from"] . "' AND transaction.delivery_date<='" . $input_param["delivery_date_to"] . "'",
+		];
+
+		if (!is_null($input_param["txn_status"])) {
+			$report_param["CONDITION"] .= " AND transaction.status=" . $input_param["txn_status"];
+		}
+		
+		if (!is_null($input_param["txn_paid"])) {
+			$report_param["CONDITION"] .= " AND transaction.paid=" . $input_param["txn_paid"];
+		}
+
+		if (!is_null($input_param["payment_method"])) {
+			$report_param["CONDITION"] .= " AND transaction.payment_method=" . $input_param["payment_method"];
+		}
+
+		$report_client = new Client("https://jasper.ribshack.info", "jasperadmin", "dsscRGC2019", "");
+		$report = $report_client->reportService()->runReport("/Reports/RAMX/SalesOrderListByDeliveryDate", "pdf", null, null, $report_param);
+
+		$this->output
+			->set_content_type('application/pdf')
+			->set_output($report);
+	}
 }
