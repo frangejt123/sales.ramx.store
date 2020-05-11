2c64336fa529f3a9850d0cb5871a2e7ab192d164
Justine Frange <justinefrange@gmail.com>
2020-05-11 02:53:58 +0000

    report

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -87,15 +87,24 @@ class Report extends CI_Controller {
 		session_start();
 
 		if (!isset($_SESSION["username"])) $this->output->set_status_header(401)->set_output("Unauthorize Access!");
+		
+		$param = $this->input->post(NULL, "true");
+		$dd_param = explode(" - ", $param["param"]);
+		$from = date("Y-m-d", strtotime($dd_param[0]));
+		$to = date("Y-m-d", strtotime($dd_param[1]));
+		
+		$status = $param["param_status"];
+		$mop = $param["param_mop"];
+		$paid = $param["param_paid"];
 
 		$input_param = [
-			"delivery_date_from" 	=> $this->input->post("delivery_date_from", TRUE), // Required Parameter
-			"delivery_date_to" 		=> $this->input->post("delivery_date_to", TRUE), // Required Parameter
-			"txn_status" 			=> $this->input->post("txn_status", TRUE),
-			"payment_method" 		=> $this->input->post("payment_method", TRUE),
-			"txn_paid" 				=> $this->input->post("txn_paid", TRUE),
+			"delivery_date_from" 	=> $from, // Required Parameter
+			"delivery_date_to" 		=> $to, // Required Parameter
+			"txn_status" 			=> $status,
+			"payment_method" 		=> $mop,
+			"txn_paid" 				=> $paid
 		];
-
+		
 		// Check required parameters
 		if (is_null($input_param["delivery_date_from"]) || is_null($input_param["delivery_date_from"])) $this->output->set_status_header(400)->set_output("Bad Request");
 
@@ -104,18 +113,19 @@ class Report extends CI_Controller {
 			"DELIVERY_DATE_TO" => $input_param["delivery_date_to"],
 			"CONDITION" => "transaction.status!=3 AND transaction.delivery_date>='" . $input_param["delivery_date_from"] . "' AND transaction.delivery_date<='" . $input_param["delivery_date_to"] . "'",
 		];
-
-		if (!is_null($input_param["txn_status"])) {
+		
+		if ($input_param["txn_status"] != "") {
 			$report_param["CONDITION"] .= " AND transaction.status=" . $input_param["txn_status"];
 		}
 		
-		if (!is_null($input_param["txn_paid"])) {
+		if ($input_param["txn_paid"] != "") {
 			$report_param["CONDITION"] .= " AND transaction.paid=" . $input_param["txn_paid"];
 		}
-
-		if (!is_null($input_param["payment_method"])) {
-			$report_param["CONDITION"] .= " AND transaction.payment_method=" . $input_param["payment_method"];
+		
+		if ($input_param["payment_method"] != "") {
+			$report_param["CONDITION"] .= " AND transaction.payment_method IN (".$input_param["payment_method"].")";
 		}
+		
 		
 		$report_client = new Client("https://jasper.ribshack.info", "jasperadmin", "dsscRGC2019", "");
 		$report = $report_client->reportService()->runReport("/Reports/RAMX/SalesByDeliveryDate", "pdf", null, null, $report_param);
@@ -123,19 +133,29 @@ class Report extends CI_Controller {
 		$this->output
 			->set_content_type('application/pdf')
 			->set_output($report);
+			
 	}
 
 	public function so_list_by_delivery_date() {
 		session_start();
 
 		if (!isset($_SESSION["username"])) $this->output->set_status_header(401)->set_output("Unauthorize Access!");
+		
+		$param = $this->input->post(NULL, "true");
+		$dd_param = explode(" - ", $param["param"]);
+		$from = date("Y-m-d", strtotime($dd_param[0]));
+		$to = date("Y-m-d", strtotime($dd_param[1]));
+		
+		$status = $param["param_status"];
+		$mop = $param["param_mop"];
+		$paid = $param["param_paid"];
 
 		$input_param = [
-			"delivery_date_from" 	=> $this->input->post("delivery_date_from", TRUE), // Required Parameter
-			"delivery_date_to" 		=> $this->input->post("delivery_date_to", TRUE), // Required Parameter
-			"txn_status" 			=> $this->input->post("txn_status", TRUE),
-			"payment_method" 		=> $this->input->post("payment_method", TRUE),
-			"txn_paid" 				=> $this->input->post("txn_paid", TRUE),
+			"delivery_date_from" 	=> $from, // Required Parameter
+			"delivery_date_to" 		=> $to, // Required Parameter
+			"txn_status" 			=> $status,
+			"payment_method" 		=> $mop,
+			"txn_paid" 				=> $paid
 		];
 
 		// Check required parameters
@@ -147,16 +167,16 @@ class Report extends CI_Controller {
 			"CONDITION" => "transaction.delivery_date>='" . $input_param["delivery_date_from"] . "' AND transaction.delivery_date<='" . $input_param["delivery_date_to"] . "'",
 		];
 
-		if (!is_null($input_param["txn_status"])) {
+		if ($input_param["txn_status"] != "") {
 			$report_param["CONDITION"] .= " AND transaction.status=" . $input_param["txn_status"];
 		}
 		
-		if (!is_null($input_param["txn_paid"])) {
+		if ($input_param["txn_paid"] != "") {
 			$report_param["CONDITION"] .= " AND transaction.paid=" . $input_param["txn_paid"];
 		}
-
-		if (!is_null($input_param["payment_method"])) {
-			$report_param["CONDITION"] .= " AND transaction.payment_method=" . $input_param["payment_method"];
+		
+		if ($input_param["payment_method"] != "") {
+			$report_param["CONDITION"] .= " AND transaction.payment_method IN (".$input_param["payment_method"].")";
 		}
 
 		$report_client = new Client("https://jasper.ribshack.info", "jasperadmin", "dsscRGC2019", "");
@@ -171,34 +191,46 @@ class Report extends CI_Controller {
 		session_start();
 
 		if (!isset($_SESSION["username"])) $this->output->set_status_header(401)->set_output("Unauthorize Access!");
+		$param = $this->input->post(NULL, "true");
+		
+		$dd_param = explode(" - ", $param["param"]);
+		$from = date("Y-m-d", strtotime($dd_param[0]));
+		$to = date("Y-m-d", strtotime($dd_param[1]));
+		
+		$trx_from = "";
+		$trx_to = "";
+		if($param["param_trxdate"] != ""){
+			$trx_param = explode(" - ", $param["param_trxdate"]);
+			$trx_from = date("Y-m-d", strtotime($trx_param[0]));
+			$trx_to = date("Y-m-d", strtotime($trx_param[1]));
+		}
+		
+		$status = $param["param_status"];
+		$mop = $param["param_mop"];
 
 		$input_param = [
-			"delivery_date_from" 	=> $this->input->post("delivery_date_from", TRUE),
-			"delivery_date_to" 		=> $this->input->post("delivery_date_to", TRUE),
-			"txn_date_from" 		=> $this->input->post("txn_date_from", TRUE),
-			"txn_date_to" 			=> $this->input->post("txn_date_to", TRUE),
-			"txn_status" 			=> $this->input->post("txn_status", TRUE),
-			"payment_method" 		=> $this->input->post("payment_method", TRUE)
+			"delivery_date_from" 	=> $from,
+			"delivery_date_to" 		=> $to,
+			"txn_date_from" 		=> $trx_from,
+			"txn_date_to" 			=> $trx_to,
+			"txn_status" 			=> $status,
+			"payment_method" 		=> $mop
 		];
 
 		$report_param = [
 			"CONDITION" => "transaction.status!=3 AND transaction.paid=1"
 		];
 
-		if (!is_null($input_param["txn_date_from"]) && !is_null($input_param["txn_date_to"])) {
+		if (($input_param["txn_date_from"] != "") && ($input_param["txn_date_to"])) {
 			$report_param["CONDITION"] .= " AND transaction.datetime>='" . $input_param["txn_date_from"] . "' AND transaction.datetime<='" . $input_param["txn_date_to"] . "'";
 		}
 
-		if (!is_null($input_param["delivery_date_from"]) && !is_null($input_param["delivery_date_to"])) {
-			$report_param["CONDITION"] .= " AND transaction.delivery_date>='" . $input_param["delivery_date_from"] . "' AND transaction.delivery_date<='" . $input_param["delivery_date_to"] . "'";
-		}
-
-		if (!is_null($input_param["txn_status"])) {
+		if ($input_param["txn_status"] != "") {
 			$report_param["CONDITION"] .= " AND transaction.status=" . $input_param["txn_status"];
 		}
-
-		if (!is_null($input_param["payment_method"])) {
-			$report_param["CONDITION"] .= " AND transaction.payment_method=" . $input_param["payment_method"];
+		
+		if ($input_param["payment_method"] != "") {
+			$report_param["CONDITION"] .= " AND transaction.payment_method IN (".$input_param["payment_method"].")";
 		}
 
 		$report_client = new Client("https://jasper.ribshack.info", "jasperadmin", "dsscRGC2019", "");
