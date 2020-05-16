3efc90e86fa11b44554f31cd8d60d3e7e82caa03
nhojener <nhojener@gmail.com>
2020-05-16 17:14:04 +0800

    Update Report.php

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -206,62 +206,71 @@ class Report extends CI_Controller {
 		session_start();
 
 		if (!isset($_SESSION["username"])) $this->output->set_status_header(401)->set_output("Unauthorize Access!");
-		$param = $this->input->post(NULL, "true");
-		
-		$from = "";
-		$to = "";
-		if(isset($param["param"]) && $param["param"] != ""){
-			$dd_param = explode(" - ", $param["param"]);
-			$from = date("Y-m-d", strtotime($dd_param[0]));
-			$to = date("Y-m-d", strtotime($dd_param[1]));
-		}
-		
-		$trx_from = "";
-		$trx_to = "";
-		if(isset($param["param_trxdate"]) && $param["param_trxdate"] != ""){
-			$trx_param = explode(" - ", $param["param_trxdate"]);
-			$trx_from = date("Y-m-d", strtotime($trx_param[0]));
-			$trx_to = date("Y-m-d", strtotime($trx_param[1]));
-		}
-		
-		$status = isset($param["param_status"]) ? $param["param_status"] : "";
-		$mop = isset($param["param_mop"]) ? $param["param_mop"] : "";
-		$driver = isset($param["param_driver"]) ? $param["param_driver"] : "";
 
 		$input_param = [
-			"delivery_date_from" 	=> $from,
-			"delivery_date_to" 		=> $to,
-			"txn_date_from" 		=> $trx_from,
-			"txn_date_to" 			=> $trx_to,
-			"txn_status" 			=> $status,
-			"payment_method" 		=> $mop,
-			"driver"				=> $driver
+			"delivery_date_from" 	=> null,
+			"delivery_date_to" 		=> null,
+			"txn_date_from" 		=> null,
+			"txn_date_to" 			=> null,
+			"txn_status" 			=> null,
+			"payment_method" 		=> null,
+			"driver"				=> null,
+			"txn_paid"				=> null,
+			"txn_unpaid"			=> null
 		];
+
+		$param = $this->input->post(NULL, "true");
+		
+		if(isset($param["param"]) && $param["param"] != "") {
+			$daterange = explode(" - ", $param["param"]);
+
+			$input_param["delivery_date_from"] = DateTime::createFromFormat("m/d/Y", $daterange[0]);
+			$input_param["delivery_date_to"] = DateTime::createFromFormat("m/d/Y", $daterange[1]);
+		}
+		
+		if(isset($param["param_trxdate"]) && $param["param_trxdate"] != "") {
+			$daterange = explode(" - ", $param["param_trxdate"]);
+
+			$input_param["txn_date_from"] = DateTime::createFromFormat("m/d/Y", $daterange[0]);
+			$input_param["txn_date_to"] = DateTime::createFromFormat("m/d/Y", $daterange[1]);
+		}
+
+		$input_param["txn_status"] = isset($param["param_status"]) && $param["param_status"] != "" ? $param["param_status"] : null;
+		$input_param["payment_method"] = isset($param["param_mop"]) && $param["param_mop"] != "" ? $param["param_mop"] : null;
+		$input_param["driver"] = isset($param["param_driver"]) && $param["param_driver"] != "" ? $param["param_driver"] : null;
+		$input_param["txn_paid"] = isset($param["param_paid"]) && $param["param_paid"] != "" ? TRUE : FALSE;
+		$input_param["txn_unpaid"] = isset($param["param_unpaid"]) && $param["param_unpaid"] != "" ? TRUE : FALSE;
 
 		$report_param = [
-			"CONDITION" => "transaction.status!=3 AND transaction.paid=1"
+			"CONDITION" => "transaction.status!=3"
 		];
 		
-		if (($input_param["delivery_date_from"] != "") && ($input_param["delivery_date_to"] != "")) {
-			$report_param["CONDITION"] .= " AND transaction.delivery_date>='" . $input_param["delivery_date_from"] . "' AND transaction.delivery_date<='" . $input_param["delivery_date_to"] . "'";
+		if (!is_null($input_param["delivery_date_from"]) && !is_null($input_param["delivery_date_to"])) {
+			$report_param["CONDITION"] .= " AND (transaction.delivery_date>='" . $input_param["delivery_date_from"]->format("Y-m-d") . "' AND transaction.delivery_date<='" . $input_param["delivery_date_to"]->format("Y-m-d") . "')";
 		}
 
-		if (($input_param["txn_date_from"] != "") && ($input_param["txn_date_to"] != "")) {
-			$report_param["CONDITION"] .= " AND transaction.datetime>='" . $input_param["txn_date_from"] . "' AND transaction.datetime<='" . $input_param["txn_date_to"] . "'";
+		if (!is_null($input_param["txn_date_from"]) && !is_null($input_param["txn_date_to"])) {
+			$report_param["CONDITION"] .= " AND (transaction.datetime>='" . $input_param["txn_date_from"]->format("Y-m-d") . "' AND transaction.datetime<='" . $input_param["txn_date_to"]->format("Y-m-d") . "')";
 		}
 
-		if ($input_param["txn_status"] != "") {
+		if (!is_null($input_param["txn_status"]))
 			$report_param["CONDITION"] .= " AND transaction.status=" . $input_param["txn_status"];
-		}
 		
-		if ($input_param["payment_method"] != "") {
+		if (!is_null($input_param["payment_method"]))
 			$report_param["CONDITION"] .= " AND transaction.payment_method IN (".$input_param["payment_method"].")";
-		}
-
-		if (!is_null($input_param["driver"]) && $input_param["driver"] != "") {
-			$report_param["CONDITION"] .= " AND driver.id=\"" . $input_param["driver"] . "\"";
-		}
 		
+		if (!is_null($input_param["driver"]))
+			$report_param["CONDITION"] .= " AND driver.id=" . $input_param["driver"];
+
+
+		if ($input_param["txn_paid"] && $input_param["txn_unpaid"]) {
+			$report_param["CONDITION"] .= "";
+		} else if ($input_param["txn_paid"]) {
+			$report_param["CONDITION"] .= " AND transaction.paid=1";
+		} else if ($input_param["txn_unpaid"]) {
+			$report_param["CONDITION"] .= " AND transaction.paid=0";
+		}
+			
 		//print_r($report_param);
 	
 		$report_client = new Client("https://jasper.ribshack.info", "jasperadmin", "dsscRGC2019", "");
@@ -315,31 +324,31 @@ class Report extends CI_Controller {
 		
 		
 		$report_param = [
-			"CONDITION" => "transaction.status!=3 AND transaction.paid=1",
+			"CONDITION" => "(transaction.status!=3 AND transaction.paid=1)",
 			"REPORT_PAYLOAD" => ""
 		];
 		
 		
 		if (!is_null($input_param["delivery_date_from"]) && !is_null($input_param["delivery_date_to"])) {
-			$report_param["CONDITION"] .= " AND transaction.delivery_date>='" . $input_param["delivery_date_from"]->format("Y-m-d") . "' AND transaction.delivery_date<='" . $input_param["delivery_date_to"]->format("Y-m-d") . "'";
+			$report_param["CONDITION"] .= " AND (transaction.delivery_date>='" . $input_param["delivery_date_from"]->format("Y-m-d") . "' AND transaction.delivery_date<='" . $input_param["delivery_date_to"]->format("Y-m-d") . "')";
 
 			$report_param["REPORT_PAYLOAD"] .= "DELIVERY DATE[" . $input_param["delivery_date_from"]->format("m/d/Y") . " - " . $input_param["delivery_date_to"]->format("m/d/Y") . "]; ";
 		}
 
 		if (!is_null($input_param["txn_date_from"]) && !is_null($input_param["txn_date_to"])) {
-			$report_param["CONDITION"] .= " AND transaction.datetime>='" . $input_param["txn_date_from"]->format("Y-m-d") . "' AND transaction.datetime<='" . $input_param["txn_date_to"]->format("Y-m-d") . "'";
+			$report_param["CONDITION"] .= " AND (DATE(transaction.datetime)>='" . $input_param["txn_date_from"]->format("Y-m-d") . "' AND DATE(transaction.datetime)<='" . $input_param["txn_date_to"]->format("Y-m-d") . "')";
 
 			$report_param["REPORT_PAYLOAD"] .= "TXN DATE[" . $input_param["txn_date_from"]->format("m/d/Y") . " - " . $input_param["txn_date_to"]->format("m/d/Y") . "]; ";
 		}
 
 		if (!is_null($input_param["payment_date_from"]) && !is_null($input_param["payment_date_to"])) {
-			$report_param["CONDITION"] .= " AND payment.payment_date>='" . $input_param["payment_date_from"]->format("Y-m-d") . "' AND payment.payment_date<='" . $input_param["payment_date_to"]->format("Y-m-d") . "'";
+			$report_param["CONDITION"] .= " AND (payment.payment_date>='" . $input_param["payment_date_from"]->format("Y-m-d") . "' AND payment.payment_date<='" . $input_param["payment_date_to"]->format("Y-m-d") . "')";
 
 			$report_param["REPORT_PAYLOAD"] .= "PAYMENT DATE[" . $input_param["payment_date_from"]->format("m/d/Y") . " - " . $input_param["payment_date_to"]->format("m/d/Y") . "]; ";
 		}
 		
 		if (!is_null($input_param["payment_method"])) {
-			$report_param["CONDITION"] .= " AND payment.payment_method IN (" . $input_param["payment_method"] . ")";
+			$report_param["CONDITION"] .= " AND (payment.payment_method IN (" . $input_param["payment_method"] . "))";
 
 			$method = "";
 
